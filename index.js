9dc10d21a12d6ff2a0b67d02fb163c39a7f5c0f8
PiotrO9 <piotr.ostrowski02@wp.pl>
2022-11-23 12:02:00 +0100

    Add randomizing one user from channel

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const { Client, GatewayIntentBits, PermissionsBitField, Permissions } = require('discord.js');
+const { Client, GatewayIntentBits, PermissionsBitField, Permissions, EmbedBuilder } = require('discord.js');
 const botApiKey = require('./key.js');
 
 const prefix = ">";
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, 
+                                GatewayIntentBits.GuildMessages, 
+                                GatewayIntentBits.MessageContent, 
+                                GatewayIntentBits.GuildMembers]});
+
+const InfoEmbed = new EmbedBuilder()
+.setTitle("Darullo INFO")
+.setDescription("Wyświetla informacje o bocie Darullo")
+.setColor("1ECC5B")
+.addFields(
+  {name: prefix + "info", value: "Wyświetla infromacje i dostępne komendy", inline: false},
+  {name: prefix + "pozdro", value: "Pozdrawia użytkownika", inline: false},
+  {name: prefix + "szambonurek", value: "Jeszcze nie działa ale będzie działać", inline: true},
+)
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -16,14 +29,35 @@ client.on("messageCreate", async(message) => {
     const command = args.shift().toLowerCase();
 
     const messageArray = message.content.split(" ");
-    const argument = messageArray.slice(1);
-    const cmd = messageArray[0];
+    // const argument = messageArray.slice(1);
+    // const cmd = messageArray[0];
 
     switch(command)
     {
+        case "info":
+            {
+                message.channel.send({ embeds: [InfoEmbed]} );
+                break;
+            }
         case "pozdro":
             {
                 message.channel.send("Siema " + message.author.toString());
+                break;
+            }
+        case "szambonurek":
+            {
+                var usersFromVoiceChannel = new Array();
+                message.guild.members.cache.filter(member => {
+                    if(!member.user.bot)
+                    {
+                        usersFromVoiceChannel.push(member.id);
+                    }
+                });
+                const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+                let targetUser = message.guild.members.fetch(usersFromVoiceChannel[random(0, usersFromVoiceChannel.length)]);
+                (await targetUser).voice.setChannel(null);
+
                 break;
             }
         default:
--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-const botKey = 'MTA0NDY3MDcyMjgwNDU1MTc4MA.G0wssv.FB9MnsI61Xt--qlvlUN3ec8jPtvVCRRu1i1ruA';
+const botKey = 'MTA0NDY3MDcyMjgwNDU1MTc4MA.G6CFgy.ieKIh9g3M5vltfirOu1S1ybRPIjyX2SWZe18os';
 module.exports = {
     botKey
 }
